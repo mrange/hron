3e8aa4334b46a438738554a3f8c57af3e900b506
mrange <marten_range@hotmail.com>
2012-11-18 22:54:21 +0100

    Added randomized hron document

--- languages/FSharp/M3.HRON.Reference/M3.HRON.Reference.Tests/Program.fs
+++ languages/FSharp/M3.HRON.Reference/M3.HRON.Reference.Tests/Program.fs
@@ -14,12 +14,89 @@ open System
 open System.IO
 open M3.HRON.Reference
 
+let min_empty_string = 0
+let max_empty_string = 2
+
+let min_string = 4
+let max_string = 10
+
+let min_member = 5
+let max_member = 10
+
+let min_value_lines = 1
+let max_value_lines = 5
+
+let max_level = 5
+
+let build_empty_char (random : Random)=
+    let c = random.Next(0,2)
+    match c with    
+        |   0 -> '\t'
+        |   1 -> ' '
+let build_empty_string (random : Random)=
+    let c = random.Next(min_empty_string, max_empty_string)
+    new string ([for i in 0..c -> build_empty_char random] |> List.toArray)
+
+let build_char (random : Random)=
+    let c = random.Next(0,25)
+    (char)(c + 65)
+
+let build_string (random : Random)=
+    let c = random.Next(min_string, max_string)
+    new string ([for i in 0..c -> build_char random] |> List.toArray)
+
+let build_value_line (random : Random)=
+    let c = random.Next(0, 10)
+    match c with
+        |   0           -> CommentLine  (build_empty_string random, build_string random)
+        |   _ when c < 4-> EmptyLine    (build_empty_string random)
+        |   _           -> ContentLine  (build_string random)
+
+let build_value_lines (random : Random)=
+    let c = random.Next(min_value_lines, max_value_lines)
+    [for i in 0..c -> build_value_line random]
+
+let rec build_member l (random : Random)=
+    let c = random.Next(0, 10)
+    match c with
+        |   0           -> Empty    (build_empty_string random)
+        |   1           -> Comment  (build_empty_string random, build_string random)
+        |   _ when c < 5-> Value    (build_string random, build_value_lines random)
+        |   _           -> Object   (build_string random, build_members l random)
+and build_members l (random : Random)= 
+    let c = random.Next(min_member, max_member)
+    if l > 0
+    then [for i in 0..c -> build_member (l - 1) random]
+    else []
+
+
 [<EntryPoint>]
 let main argv = 
-    let input = File.ReadAllText(@"..\..\..\..\..\..\reference-data\helloworld.hron")     
+    let random = new Random (19740531)
+
+//    let x = build_members max_level random
+//    let y = HRONParser.to_string x
+//    File.WriteAllText ("random.hron", y)
+
+    let path  = Path.GetFullPath(@"..\..\..\..\..\..\reference-data\random.hron")
+    let input = File.ReadAllText(path)
+    printf "Loaded: %s\r\n" path     
     let result = Parser.parse HRONParser.p_hron input
     match result with 
-        |   Success (doc, ps)   ->  printf "Success\r\n%s" (HRONParser.to_string doc)
-        |   Failure (_,_)       ->  printf "Failure"
+        |   Success (doc, ps)   ->  
+            let output = HRONParser.to_string doc
+            let l1              = input.Length
+            let l2              = input.Length
+            let lm              = min l1 l2
+            let mutable first   = -1
+            for i in 0..(lm - 1) do
+                if first = -1 && input.[i] <> output.[i] 
+                    then first <- i
+            if first = -1 && l1 = l2
+            then printf "Successfully read, parsed and serialized\r\n"
+            elif first = -1
+            then printf "Length mismatch %d, %d\r\n" l1 l2
+            else printf "First mismatch@ %d\r\n" first 
+        |   Failure (_,_)       ->  printf "Failed to parse\r\n"
     
     0 
