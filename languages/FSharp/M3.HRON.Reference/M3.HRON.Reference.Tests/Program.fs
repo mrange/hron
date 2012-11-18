ba38e4818132144922c3da18e0936fb88eba259b
mrange <marten_range@hotmail.com>
2012-11-18 14:05:14 +0100

    Added FSharp reference parser, the purpose of this parser is to have an implementation that is a close to the EBNF grammar as possible where performance is of secondary interest. For a performanant .NET parser look to the C# implementation.

---
+++ languages/FSharp/M3.HRON.Reference/M3.HRON.Reference.Tests/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+open System.IO
+open M3.HRON.Reference
+
+[<EntryPoint>]
+let main argv = 
+    let input = File.ReadAllText(@"..\..\..\..\..\..\reference-data\simple.hron")     
+    let result = Parser.parse HRONParser.p_hron input
+    match result with 
+        |   Success (doc, ps)   ->  Console.WriteLine("Success")
+        |   Failure (_,_)       ->  Console.WriteLine("Failure")
+    0 
---
+++ languages/FSharp/M3.HRON.Reference/M3.HRON.Reference/HRONParser.fs
@@ -0,0 +1,28 @@
+﻿namespace M3.HRON.Reference
+
+open System
+open System.Diagnostics
+open Parser
+
+type Member         =
+    |   Value   of string*string list
+    |   Object  of string*Member list
+
+
+type HRON           = Member list
+
+module HRONParser =
+
+    let p_value_tag     = p_char '='
+    let p_object_tag    = p_char '@'
+
+    let p_string        = p_many (p_any_char >>! p_eol) >>? (fun cs -> new string(List.toArray cs))
+
+    let p_value_line    = p_indention >>. p_string .>> p_eol
+    let p_value         = p_indention >>. p_value_tag >>. p_string .>> p_eol .>> p_indent >> (p_many p_value_line) .>> p_dedent >>? Value
+
+    let rec p_object ps = (p_indention >>. p_object_tag >>. p_string .>> p_eol .>> p_indent >> (p_many p_member) .>> p_dedent >>? Object) ps
+    and p_member ps     = (p_choose [p_value; p_object]) ps
+
+    let p_hron  = p_many p_member
+
