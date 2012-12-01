19bc4e98823125a29dc8ecf7666fe96bda8bc8b9
mrange <marten_range@hotmail.com>
2012-12-01 17:23:50 +0100

    Updated license info

--- languages/Cpp/m3_hron/M3.HRON.Generator/Parser.cs
+++ languages/Cpp/m3_hron/M3.HRON.Generator/Parser.cs
@@ -1,8 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// ----------------------------------------------------------------------------------------------
+// Copyright (c) Mårten Rånge.
+// ----------------------------------------------------------------------------------------------
+// This source code is subject to terms and conditions of the Microsoft Public License. A 
+// copy of the license can be found in the License.html file at the root of this distribution. 
+// If you cannot locate the  Microsoft Public License, please send an email to 
+// dlr@microsoft.com. By using this source code in any fashion, you are agreeing to be bound 
+//  by the terms of the Microsoft Public License.
+// ----------------------------------------------------------------------------------------------
+// You must not remove this notice, or any other, from this software.
+// ----------------------------------------------------------------------------------------------
 
 namespace M3.HRON.Generator.Parser
 {
