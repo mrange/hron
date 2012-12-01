b25c93102c9792bd075083601db7d01dcb8ac3bc
mrange <marten_range@hotmail.com>
2012-12-01 17:21:48 +0100

    Continuous work on the generated C/C# parser

---
+++ languages/Cpp/m3_hron/M3.HRON.Generator/Parser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M3.HRON.Generator.Parser
+{
+    partial class Scanner
+    {
+        int indention;
+        int lineNo;
+        partial void Partial_ComputeNewState(
+            char current, 
+            ParserState from, 
+            ParserState to, 
+            ParserStateTransition transition, 
+            ref ParserResult result, 
+            ref ParserState newState
+            )
+        {
+
+        }
+    }
+}
