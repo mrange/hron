c7a1a2ee02e34ad7407360978c4bb074d0c847de
mrange <marten_range@hotmail.com>
2012-12-02 13:01:33 +0100

    Further additions to get generated c/c# parser up and running

--- languages/Cpp/m3_hron/M3.HRON.Generator/Parser.cs
+++ languages/Cpp/m3_hron/M3.HRON.Generator/Parser.cs
@@ -10,22 +10,69 @@
 // You must not remove this notice, or any other, from this software.
 // ----------------------------------------------------------------------------------------------
 
+// ReSharper disable InconsistentNaming
+using System;
+using System.Text;
+using M3.HRON.Generator.Source.Common;
+
 namespace M3.HRON.Generator.Parser
 {
+
+    interface IVisitor
+    {
+        void Document_Begin();
+        void Document_End();
+
+        void Object_Begin(SubString name);
+        void Object_End(SubString name);
+
+        void Value_Begin(SubString name);
+        void Value_Line(SubString name);
+        void Value_End(SubString name);
+    }
+
     partial class Scanner
     {
-        int indention;
-        int lineNo;
-        partial void Partial_ComputeNewState(
-            char current, 
-            ParserState from, 
-            ParserState to, 
-            ParserStateTransition transition, 
-            ref ParserResult result, 
-            ref ParserState newState
-            )
-        {
+        bool m_isBuildingValue;
+        int m_indention;
+        int m_expectedIndention;
+        int m_lineNo;
+        string m_current;
 
+        partial void Partial_BeginLine(string l)
+        {
+            m_indention = 0;
+            ++m_lineNo;
+            m_current = l;
+        }
+
+        partial void Partial_StateChoice(char current, ParserStateChoice choice, ParserState from, ref ParserState to)
+        {
+            switch (choice)
+            {
+                case ParserStateChoice.From_Indention__Choose_TagExpected_ValueLine_OtherValueLine_Error:
+
+                    if (!m_isBuildingValue)
+                    {
+                        
+                    }
+
+                    break;
+                default:
+                    to = ParserState.Error;
+                    break;
+            }
+        }
+
+        partial void Partial_StateTransition(char current, ParserState from, ParserState to, ParserStateTransition transition, ref ParserResult result)
+        {
+            switch (to)
+            {
+                case ParserState.Error:
+                default:
+                    result = ParserResult.Error;
+                    return;
+            }
         }
     }
 }
