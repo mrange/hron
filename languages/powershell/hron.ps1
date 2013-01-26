7f8542928a55225cec1b38b6cbc06f925a38e79e
Daniel Brännström <danielbrannstrom@hotmail.com>
2013-01-26 15:21:11 +0100

    supporting comments and empty lines correctly

--- languages/powershell/hron.ps1
+++ languages/powershell/hron.ps1
@@ -1,10 +1,32 @@
-﻿function Parse-String([ref]$lines, $indent)
+﻿function Eat-Line([ref]$lines)
+{ 
+    $line = $lines.Value[0]
+    Write-Debug ">>> $line"
+    $lines.Value = $lines.Value[1..($lines.Value.Length)] 
+}
+
+function Parse-String([ref]$lines, $indent)
 {
     $sb = New-Object System.Text.StringBuilder
-    while($lines.Value[0] -match "(?<indent>^\t*)(?<value>.*)" -and $matches.indent.Length -eq $indent)
+    while($lines.Value.Count -gt 0)
     {
-        $lines.Value = $lines.Value[1..($lines.Value.Length)]
-        $sb.AppendLine($matches.value) | Out-Null
+        $line = $lines.Value[0]
+        if ($line -match "^(?<indent>^\t*)(?<value>.*)")
+        {
+            if (($matches.indent.Length -eq $indent) -or [string]::IsNullOrEmpty($matches.value.Trim()))
+            {
+                Eat-Line $lines
+                $sb.AppendLine($matches.value) | Out-Null
+            }
+            else
+            {
+                break;
+            }
+        }
+        else
+        {
+            throw "Parse error; expecting empty line or text"
+        }
     }
     return $sb.ToString()
 }
@@ -30,40 +52,63 @@ function AddOrExtend-Member($object, $member, $value)
 
 function Parse-Members([ref]$lines, $object, $indent)
 {
-    while
-        (
-            ($lines.Value.Count -gt 0) -and
-            ($lines.Value[0] -match "(?<indent>^\t*)(?<controlchar>@|=)(?<membername>.*)") -and 
-            ($indent -eq $matches.indent.Length)
-        )
+    $margin = New-Object string($indent)
+    Write-Debug "$margin Beginning of object"
+    while($lines.Value.Count -gt 0)
     {
-        $lines.Value = $lines.Value[1..($lines.Value.Length)]
-        $value = $null
-        switch($matches.controlchar)
+        $line = $lines.Value[0]
+        if ($line -match "^\s*$")
         {
-            "@" 
+            Write-Debug "$margin Empty line"
+            Eat-Line $lines
+        }      
+        elseif ($line -match "^\s*#")
+        {
+            Write-Debug "$margin Comment"
+            Eat-Line $lines
+        }
+        elseif ($line -match "^(?<indent>^\t*)(?<controlchar>@|=)(?<membername>.*)")
+        {                       
+            if ($indent -eq $matches.indent.Length)
             {
-                $value = New-Object PSObject
-                Parse-Members $lines $value ($indent+1)
+                Write-Debug "$margin Member $($matches.membername)"
+                Eat-Line $lines
+                $value = $null
+                switch($matches.controlchar)
+                {
+                    "@" 
+                    {
+                        $value = New-Object PSObject
+                        Parse-Members $lines $value ($indent+1)
+                    }
+                    "="
+                    {
+                        $value = Parse-String $lines ($indent+1)
+                    }
+                }
+
+                AddOrExtend-Member $object $matches.membername $value
             }
-            "="
+            else
             {
-                $value = Parse-String $lines ($indent+1)
+                Write-Debug "$margin End of object reached"
+                break;
             }
         }
-
-        AddOrExtend-Member $object $matches.membername $value
+        else
+        {
+            throw "Parse error; expecting empty line, comment or member declaration"
+        }
     }
 }
 
-function ConvertFrom-HRON($text_and_comments)
+function ConvertFrom-HRON($lines)
 {
-    $lines = $text_and_comments | ? { !$_.StartsWith("#") } | ? { ![string]::IsNullOrEmpty($_.Trim()) }
     $result = New-Object psobject
     Parse-Members ([ref]$lines) $result 0
     return $result
 }
 
-$text = Get-Content .\sample.hron
+$text = Get-Content ..\..\reference-data\helloworld.hron
 $x = ConvertFrom-HRON $text
 $x
