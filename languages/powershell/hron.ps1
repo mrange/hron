77e4c93221164d7c507acf9e03dc294c8d2369c0
Daniel Brännström <danielbrannstrom@hotmail.com>
2013-01-26 15:58:49 +0100

    now parsing random reference file

--- languages/powershell/hron.ps1
+++ languages/powershell/hron.ps1
@@ -1,7 +1,10 @@
-﻿function Eat-Line([ref]$lines)
+﻿$script:linecount = 1
+
+function Eat-Line([ref]$lines)
 { 
+    $script:linecount++;
     $line = $lines.Value[0]
-    Write-Debug ">>> $line"
+    Write-Debug "$($script:linecount)>>> $line"
     $lines.Value = $lines.Value[1..($lines.Value.Length)] 
 }
 
@@ -11,21 +14,34 @@ function Parse-String([ref]$lines, $indent)
     while($lines.Value.Count -gt 0)
     {
         $line = $lines.Value[0]
-        if ($line -match "^(?<indent>^\t*)(?<value>.*)")
+        if ($line -match "^\s*$")
         {
-            if (($matches.indent.Length -eq $indent) -or [string]::IsNullOrEmpty($matches.value.Trim()))
+            Eat-Line $lines
+            $sb.AppendLine() | Out-Null
+        }        
+        else
+        {
+            $pattern = "^(?<indent>^\t{$indent})(?<value>.*)"
+            if ($line -match $pattern)
+            {
+                if (($matches.indent.Length -eq $indent) -or [string]::IsNullOrEmpty($matches.value.Trim()))
+                {
+                    Eat-Line $lines
+                    $sb.AppendLine($matches.value) | Out-Null
+                }
+                else
+                {
+                    break;
+                }
+            }
+            elseif ($line -match "^\s*#")
             {
                 Eat-Line $lines
-                $sb.AppendLine($matches.value) | Out-Null
             }
             else
             {
                 break;
             }
-        }
-        else
-        {
-            throw "Parse error; expecting empty line or text"
         }
     }
     return $sb.ToString()
@@ -59,20 +75,20 @@ function Parse-Members([ref]$lines, $object, $indent)
         $line = $lines.Value[0]
         if ($line -match "^\s*$")
         {
-            Write-Debug "$margin Empty line"
             Eat-Line $lines
+            Write-Debug "$margin Empty line"
         }      
         elseif ($line -match "^\s*#")
         {
-            Write-Debug "$margin Comment"
             Eat-Line $lines
+            Write-Debug "$margin Comment"
         }
         elseif ($line -match "^(?<indent>^\t*)(?<controlchar>@|=)(?<membername>.*)")
         {                       
             if ($indent -eq $matches.indent.Length)
             {
-                Write-Debug "$margin Member $($matches.membername)"
                 Eat-Line $lines
+                Write-Debug "$margin Member $($matches.membername)"
                 $value = $null
                 switch($matches.controlchar)
                 {
@@ -97,7 +113,24 @@ function Parse-Members([ref]$lines, $object, $indent)
         }
         else
         {
-            throw "Parse error; expecting empty line, comment or member declaration"
+            throw "Parse error on line $($script:linecount); expecting empty line, comment or member declaration"
+        }
+    }
+}
+
+function Parse-Header([ref]$lines)
+{
+    while($lines.Value.Count -gt 0)
+    {
+        $line = $lines.Value[0]
+        if ($line -match "^\s*!")
+        {
+            Eat-Line $lines
+            Write-Debug "Directive"            
+        }
+        else
+        {
+            break;
         }
     }
 }
@@ -105,10 +138,33 @@ function Parse-Members([ref]$lines, $object, $indent)
 function ConvertFrom-HRON($lines)
 {
     $result = New-Object psobject
-    Parse-Members ([ref]$lines) $result 0
+    $linesRef = [ref]$lines
+    Parse-Header $linesRef
+    Parse-Members $linesRef $result 0
     return $result
 }
 
-$text = Get-Content ..\..\reference-data\helloworld.hron
-$x = ConvertFrom-HRON $text
-$x
+function Test-HelloWorld
+{
+    $text = Get-Content ..\..\reference-data\helloworld.hron
+    $x = ConvertFrom-HRON $text
+    Write-Host "Common.LogPath: " $x.Common.LogPath 
+    Write-Host "Common.Welcomemessage: " $x.Common.WelcomeMessage
+    foreach($conn in $x.DataBaseConnection)
+    {
+        Write-Host "Databaseconnection.Name: " $conn.Name
+        Write-Host "Databaseconnection.Timeout: " $conn.Timeout
+        Write-Host "Databaseconnection.User.UserName: " $conn.User.UserName
+        Write-Host "Databaseconnection.User.Password: " $conn.User.Password
+    }
+}
+
+function Test-Random
+{
+    $text = Get-Content ..\..\reference-data\random.hron
+    $x = ConvertFrom-HRON $text
+    write-host $x
+}
+
+#Test-HelloWorld
+Test-Random
