6895c185fd192535ccd66c0e6e18095a6366db68
Daniel Brännström <danielbrannstrom@hotmail.com>
2015-02-28 10:16:46 +0100

    Added script for downloading mongoose

---
+++ languages/javascript/utils/get-mongoose.ps1
@@ -0,0 +1,23 @@
+#Requires –Version 3
+param($targetFolder)
+
+$downloadUrl = "http://cesanta.com/cgi-bin/api.cgi?act=dl&id=7NBB80IL1YZZ9ZXN&os=win" 
+if (!$targetFolder) {
+    $targetFolder = Join-Path $PSScriptRoot ".." | Resolve-Path
+}
+$target = Join-Path $targetFolder mongoose-free-5.5.exe
+
+if (Test-Path $target) {
+    Write-Host "Mongoose already exists. Nothing done"
+    return
+}
+
+Write-Host "Downloading Mongoose..." -NoNewline
+Invoke-WebRequest -Uri $downloadUrl -OutFile $target
+Write-Host "Done."
+
+Write-Host "Unblock file to allow execution"
+Unblock-File $target
+
+Write-Host "Completed successfully"
+
