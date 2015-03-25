cb0d2d56def4627aa791be1611378cf7f473f403
Daniel Brännström <danielbrannstrom@hotmail.com>
2015-03-25 22:05:49 +0100

    Replaced testing framework for javascript lib

--- languages/javascript/Gruntfile.js
+++ languages/javascript/Gruntfile.js
@@ -7,6 +7,10 @@ module.exports = function( grunt ) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    nodeunit: {
+      all: ['test-nodeunit.js']
+    }, 
+
     jshint: {
       src: [ 'hron.js' ],
       options: grunt.file.readJSON('.jshintrc')
@@ -27,8 +31,10 @@ module.exports = function( grunt ) {
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
   grunt.registerTask( 'default', [
+    'nodeunit',
     'jshint',
     'uglify'
   ]);
