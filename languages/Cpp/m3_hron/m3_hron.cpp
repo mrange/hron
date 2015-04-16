4118619e56485fba1cb426b785693bd996fb9ee5
mrange <marten_range@hotmail.com>
2015-04-16 19:01:56 +0200

    Moved HRON cpp files into child folder

--- languages/Cpp/m3_hron/m3_hron.cpp
+++ languages/Cpp/m3_hron/m3_hron.cpp
@@ -22,7 +22,7 @@
 #include <string>
 #include <vector>
 // -----------------------------------------------------------------------------
-#include "hron_parser.hpp"
+#include "hron_parser/hron_parser.hpp"
 // -----------------------------------------------------------------------------
 namespace
 {
